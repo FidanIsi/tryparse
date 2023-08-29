38915153120b803545c325c78dac4b8ddd601090
fidan <fidanji@code.edu.az>
2023-08-29 15:01:36 +0400

    Parse

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static bool CustomTryParse(string input, out int res)
+        {
+            res = 0;
+
+            try
+            {
+                res = int.Parse(input);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter a string:");
+
+            string input = Console.ReadLine();
+
+            if (CustomTryParse(input, out int res))
+            {
+                Console.WriteLine(res);
+            }
+            else
+            {
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
